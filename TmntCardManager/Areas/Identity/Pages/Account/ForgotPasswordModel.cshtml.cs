6c12f63c470652e5cda9fb5ae56580a58214240f
Bileysh <bileysh06@knu.ua>
2026-04-04 22:06:18 +0300

    Add "ForgotPassword"

---
+++ TmntCardManager/Areas/Identity/Pages/Account/ForgotPasswordModel.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using TmntCardManager.Models;
+
+namespace TmntCardManager.Areas.Identity.Pages.Account
+{
+    public class ForgotPasswordModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public ForgotPasswordModel(UserManager<User> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "Будь ласка, введіть Email")]
+            [EmailAddress(ErrorMessage = "Невірний формат Email")]
+            public string Email { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+                
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                {
+                    return RedirectToPage("./ForgotPasswordConfirmationModel");
+                }
+
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                
+                var callbackUrl = Url.Page(
+                    "./ResetPasswordModel",
+                    pageHandler: null,
+                    values: new { area = "Identity", code },
+                    protocol: Request.Scheme);
+
+                await _emailSender.SendEmailAsync(
+                    Input.Email,
+                    "Відновлення доступу (TMNT)",
+                    $"Ви запросили скидання пароля. <br><br> Будь ласка, створіть новий пароль, <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>натиснувши на це посилання</a>.");
+
+                return RedirectToPage("./ForgotPasswordConfirmationModel");
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ TmntCardManager/Areas/Identity/Pages/Account/ResetPasswordModel.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using TmntCardManager.Models; 
+
+namespace TmntCardManager.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ResetPasswordModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ResetPasswordModel(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "Потрібно ввести Email")]
+            [EmailAddress(ErrorMessage = "Невірний формат Email")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Введіть новий пароль")]
+            [StringLength(100, ErrorMessage = "{0} має бути від {2} до {1} символів.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Підтвердіть пароль")]
+            [Compare("Password", ErrorMessage = "Паролі не співпадають.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Code { get; set; } 
+        }
+
+        public IActionResult OnGet(string code = null)
+        {
+            if (code == null)
+            {
+                return BadRequest("Для скидання пароля потрібен спеціальний код (токен) з листа.");
+            }
+            else
+            {
+                Input = new InputModel
+                {
+                    Code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code))
+                };
+                return Page();
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var user = await _userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, Input.Code, Input.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return Page();
+        }
+    }
+}
